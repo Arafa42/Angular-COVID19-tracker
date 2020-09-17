191129bc7088077a92c7be5a434ea036f89c39ff
Arafa Yoncalik <55528241+Arafa42@users.noreply.github.com>
2020-09-17 21:14:31 +0200

    today data +layout using semantic-ui

--- COVID-19_TRACKER/covid19-tracker/src/app/components/home/home.component.ts
+++ COVID-19_TRACKER/covid19-tracker/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataSummary } from 'src/app/models/global-data';
+import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  totalConfirmed = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  Gdata : DataSummary[];
+
+  constructor(private data: DataServiceService) { }
 
   ngOnInit(): void {
-  }
 
+    this.data.getData().subscribe({
+      next: (result)=>{        
+      console.log(result);
+      this.Gdata = result;
+      result.forEach(cs => {
+        if(!Number.isNaN(cs.confirmed)){
+        this.totalConfirmed += cs.confirmed;
+        this.totalDeaths += cs.deaths;
+        this.totalRecovered += cs.recovered;
+        }
+      })    
+      }
+    })
+
+  }
 }
--- COVID-19_TRACKER/covid19-tracker/src/app/services/data-service.service.ts
+++ COVID-19_TRACKER/covid19-tracker/src/app/services/data-service.service.ts
@@ -1,10 +1,49 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {map} from "rxjs/operators";
+import { DataSummary } from '../models/global-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
-  constructor(http:HttpClient) { }
+private dataURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_daily_case_updates/02-14-2020_1123.csv";
+
+  constructor(private http:HttpClient) { }
+
+getData(){
+return this.http.get(this.dataURL, {responseType: "text"}).pipe(
+  map(res => {  
+    let rows = res.split("\n");
+    let raw = {};
+    rows.splice(0,1);
+    rows.forEach(row=>{
+    let cols = row.split(/,(?=\S)/);
+
+
+    let cs = {
+        country: cols[1],
+        confirmed: +cols[3],
+        deaths: +cols[4],
+        recovered: +cols[5]    
+    };
+ 
+    let temp : DataSummary = raw[cs.country];
+    
+    if(temp){
+      temp.country = cs.country + temp.country;
+      temp.confirmed = cs.confirmed + temp.confirmed;
+      temp.deaths = cs.deaths + temp.deaths;
+      temp.recovered = cs.recovered + temp.recovered;  
+      
+      raw[cs.country] = temp;
+    }
+    else{
+      raw[cs.country] = cs;
+    }
+    })
+    return <DataSummary[]>Object.values(raw);}))
+}
+
 }
